f3509048e4fee3818351e1d43fe4e02508ee4972
w9617178 <w9617178@live.tees.ac.uk>
2023-11-16 17:59:16 +0000

    Initial commit

---
+++ app/src/main/java/com/example/notesapp/Models/notes.kt
@@ -0,0 +1,22 @@
+package com.example.notesapp.Models
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+
+
+Parcelize
+@Entity(tableName = "Notes")
+class notes(
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
+    var title: String,
+    var subtitle: String,
+    var notes:String,
+    var date:String,
+    var priority:String
+) : Parcelable
+
